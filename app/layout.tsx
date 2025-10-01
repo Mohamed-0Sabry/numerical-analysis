a1ba9ee4ed28f66d767ed93dc441ace43f0431b2
Mohamed Sabry <mohamed.2520076@stemelsadat.moe.edu.eg>
2025-10-01 20:51:27 +0300

    Added: Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +36,8 @@ export default function RootLayout({
         <footer className="border-t bg-card/50 backdrop-blur-sm py-4 text-center text-lg">
           © {new Date().getFullYear()} Sabry — Created with ❤️ so you can solve with ease.
         </footer>
+
+        <Analytics />
       </body>
     </html>
   );
